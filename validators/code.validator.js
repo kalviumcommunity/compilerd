d9b8e385b1ce09ba05ed1e312f13360b66393370
Anirudh Panwar <anirudhpanwar@Anirudhs-MacBook-Pro.local>
2023-12-26 15:03:01 +0530

    open ai functionality addition

--- validators/code.validator.js
+++ validators/code.validator.js
@@ -3,7 +3,18 @@ const Joi = require('joi')
 const isValidForExecute = async (body) => {
     const schema = Joi.object({
         language: Joi.string().required(),
-        script: Joi.string().required(),
+        script: Joi.string()
+            .when('language', {
+                is: 'promptv1',
+                then: Joi.optional(),
+                otherwise: Joi.required()
+            }),
+        prompt: Joi.string()
+            .when('language', {
+                is: 'promptv1',
+                then: Joi.required(),
+                otherwise: Joi.optional()
+            }),
         hasInputFiles: Joi.bool(),
         args: Joi.string(),
         stdin: Joi.string(),
