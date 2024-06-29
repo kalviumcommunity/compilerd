16fe8b132a80bbf69fff6775fb86034ff580efa3
Madhan Krithick <121656398+madhankrithick@users.noreply.github.com>
2024-06-29 15:16:13 +0530

    Update code.router.js

--- routers/code.router.js
+++ routers/code.router.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const codeController = require('../controllers/code.controller');
 
-router.post('/execute', codeController.executeCode);
+// Import the validator middleware
+const { validateCodeInput } = require('../controllers/code.controller');
+
+router.post('/execute', validateCodeInput, codeController.executeCode);
 router.post('/save', codeController.saveVersion);
 router.get('/versions', codeController.getVersions);
 router.post('/retrieve', codeController.retrieveVersion);
