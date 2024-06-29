a8c996bfbe81c9aa0c7f8c2448c1f37197b3fdb5
Madhan Krithick <121656398+madhankrithick@users.noreply.github.com>
2024-06-29 15:09:44 +0530

    Update code.router.js

--- routers/code.router.js
+++ routers/code.router.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const codeController = require('../controllers/code.controller')
+const express = require('express');
+const router = express.Router();
+const codeController = require('../controllers/code.controller');
 
-router.post('/execute', [], codeController.execute)
-module.exports = router
+router.post('/execute', codeController.executeCode);
+router.post('/save', codeController.saveVersion);
+router.get('/versions', codeController.getVersions);
+router.post('/retrieve', codeController.retrieveVersion);
+
+module.exports = router;
