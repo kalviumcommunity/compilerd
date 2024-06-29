fe9869ed04d463da22107ae34ea034249d7a2c87
Madhan Krithick <121656398+madhankrithick@users.noreply.github.com>
2024-06-29 15:44:45 +0530

    Update logger.js

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,14 +1,33 @@
-const winston = require('winston')
+const winston = require('winston');
 
+// Define logger configuration
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
-        winston.format.errors({ stack: true }),
-        winston.format.json(),
+        winston.format.timestamp(),
+        winston.format.json()
     ),
     transports: [
         new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' })
     ],
-})
+});
 
-module.exports = { logger }
+// Middleware function to log requests
+function requestLogger(req, res, next) {
+    logger.info(${req.method} ${req.originalUrl} - ${req.ip});
+    next();
+}
+
+// Error handler middleware to log errors
+function errorHandler(err, req, res, next) {
+    logger.error(${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip});
+    next(err);
+}
+
+module.exports = {
+    logger,
+    requestLogger,
+    errorHandler,
+};
