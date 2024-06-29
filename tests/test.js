1a0628807cb9d9fd842d87bc496b9117f01302d6
Madhan Krithick <121656398+madhankrithick@users.noreply.github.com>
2024-06-29 14:36:39 +0530

    Update api.test.js

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,17 @@
-const axios = require('axios')
-const { testCases } = require('./data/testJson')
-const { describe, expect, it } = require('@jest/globals')
+const axios = require('axios');
+const { testCases } = require('./data/testJson');
+const { describe, expect, it } = require('@jest/globals');
 
-const ENDPOINT = process.env.ENDPOINT || 'http://localhost:3000/api/execute/'
+const ENDPOINT = process.env.ENDPOINT || 'http://localhost:3000/api/execute/';
 
 describe('Tests', () => {
     for (const testCase of testCases) {
         it(testCase.name, async () => {
-            const response = await axios.post(ENDPOINT, testCase.reqObject)
+            const response = await axios.post(ENDPOINT, testCase.reqObject);
             if (typeof response.data.output === 'object') {
-                expect(response.data.output.score).toBeDefined()
-                expect(response.data.output.rationale.positives).toBeDefined()
-                expect(response.data.output.rationale.negatives).toBeDefined()
-                expect(response.data.output.points).toBeDefined()
+                expect(response.data.output.score).toBeDefined();
+                expect(response.data.output.rationale.positives).toBeDefined();
+                expect(response.data.output.rationale.negatives).toBeDefined();
+                expect(response.data.output.points).toBeDefined();
             } else {
-                expect(response).toHaveProperty('data.output', testCase.expectedResponse.val)
-            }
-            expect(response).toHaveProperty('status', testCase.expectedResponse.status)
-            expect(response).toHaveProperty('data.error', testCase.expectedResponse.error)
-        }, 15000)
-    }
-})
+                expect(response).toHaveProperty('data.output', testCase.expectedResponse.val);
