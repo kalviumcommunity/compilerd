6224df5df19a47c70c8c8d39eac73736fa667b78
Ayush200230 <humbleayush30@gmail.com>
2024-06-30 19:23:24 +0530

    updated frontend

--- frontend/utils/languageOptions.js
+++ frontend/utils/languageOptions.js
@@ -1,7 +1,8 @@
 export const languageOptions = [
-  { value: "python", label: "Python", monaco: "python" },
-  { value: "cpp", label: "C++", monaco: "cpp" },
-  { value: "ruby", label: "Ruby", monaco: "ruby" },
-  { value: "java", label: "Java", monaco: "java" },
-  { value: "nodejs", label: "Node.js", monaco: "javascript" },
-];
+    { value: 'python', label: 'Python', monaco: 'python' },
+    { value: 'cpp', label: 'C++', monaco: 'cpp' },
+    { value: 'ruby', label: 'Ruby', monaco: 'ruby' },
+    { value: 'java', label: 'Java', monaco: 'java' },
+    { value: 'nodejs', label: 'Node.js', monaco: 'javascript' },
+    { value: 'golang', label: 'GoLang', monaco: 'golang' },
+]
