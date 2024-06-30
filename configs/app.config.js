cf1ea033d529483a79fda8e01f28261ba9b471a4
Abhigyan Kashyap <abhigyankashyap04@gmail.com>
2024-07-01 04:49:48 +0530

    changes

--- configs/app.config.js
+++ configs/app.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     multifile: {
         staticServerPath: "/tmp/submission/",
+        
         submissionFileDownloadPath: "/tmp/submission.json",
         workingDir: "/tmp/submission/",
         jasminePort: process.env.JASMINE_PORT || 8888,
--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
+#!/bin/sh
+
+# Check if OUTBOUND_TRAFFIC_ALLOW is not set or is set to false, then drop outbound traffic on eth2
 if [ -z "${OUTBOUND_TRAFFIC_ALLOW}" ] || [ "${OUTBOUND_TRAFFIC_ALLOW}" = "false" ]; then
     iptables -I OUTPUT -o eth2 -j DROP
 fi
-su -s /bin/sh runner -c 'node --expose-gc /usr/bin/server.js'
+
+# Switch to the runner user to run the node application
+exec su -s /bin/sh runner -c 'node --expose-gc /usr/bin/server.js'
