d77d2034673d32fd81ad8ca4ae3190bd7317bb25
Ritik singh pundhir <rp4563@srmist.edu.in>
2024-07-01 02:00:36 +0530

    Update supportedLanguages.js

--- enums/supportedLanguages.js
+++ enums/supportedLanguages.js
@@ -9,8 +9,8 @@ module.exports = {
     PROMPTV2: 'promptv2',
     MULTIFILE: 'multifile',
     SQLITE3: 'sqlite3',
-    GO, // Added Go 
-    PHP, // Added Php 
-    RUST, // Added rust 
+    GO: 'go', // Added Golang 
+    PHP: 'php', // Added Php 
+    RUST: 'rust', // Added Rust 
 
 }
