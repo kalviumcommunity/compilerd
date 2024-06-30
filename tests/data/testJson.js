727c47b7f1ef93dd59cc40f7bce16afe6654b03e
Vibhuti Joshi <vp1373@srmist.edu.in>
2024-06-30 15:09:24 +0530

    new test cases

--- tests/data/testJson.js
+++ tests/data/testJson.js
@@ -18,6 +18,71 @@ const testCases = [
         },
     },
     {
+    name: "cpp : simple arithmetic",
+    reqObject: {
+        language: "cpp",
+        script: 
+            "#include<bits/stdc++.h>\n" +
+            "using namespace std;\n" +
+            "int main(){\n" +
+            "    int a = 5, b = 10;\n" +
+            "    cout << (a + b);\n" +
+            "    return 0;\n" +
+            "}\n"
+    },
+    expectedResponse: {
+        val: "15",
+        status: 200,
+        error: 0
+    }
+},
+{
+    name: "cpp : conditional statements",
+    reqObject: {
+        language: "cpp",
+        script: 
+            "#include<bits/stdc++.h>\n" +
+            "using namespace std;\n" +
+            "int main(){\n" +
+            "    int a = 10;\n" +
+            "    if (a > 5) {\n" +
+            "        cout << \"a is greater than 5\";\n" +
+            "    } else {\n" +
+            "        cout << \"a is not greater than 5\";\n" +
+            "    }\n" +
+            "    return 0;\n" +
+            "}\n"
+    },
+    expectedResponse: {
+        val: "a is greater than 5",
+        status: 200,
+        error: 0
+    }
+},
+{
+    name: "cpp : for loop",
+    reqObject: {
+        language: "cpp",
+        script: 
+            "#include<bits/stdc++.h>\n" +
+            "using namespace std;\n" +
+            "int main(){\n" +
+            "    for (int i = 0; i < 5; ++i) {\n" +
+            "        cout << i << \" \";\n" +
+            "    }\n" +
+            "    return 0;\n" +
+            "}\n"
+    },
+    expectedResponse: {
+        val: "0 1 2 3 4 ",
+        status: 200,
+        error: 0
+    }
+},
+
+
+
+    {
         name: 'cpp : print stdin',
         reqObject: {
             language: 'cpp',
@@ -40,6 +105,7 @@ const testCases = [
         },
 
     },
+
     {
         name: 'nodejs : hello world',
         reqObject: {
@@ -71,6 +137,67 @@ const testCases = [
         },
     },
     {
+        name: "nodejs : conditional statements",
+        reqObject: {
+            language: "nodejs",
+            script: 
+                "let a = 10;\n" +
+                "if (a > 5) {\n" +
+                "    console.log('a is greater than 5');\n" +
+                "} else {\n" +
+                "    console.log('a is not greater than 5');\n" +
+                "}"
+        },
+        expectedResponse: {
+            val: "a is greater than 5",
+            status: 200,
+            error: 0
+        }
+    },
+
+    {
+        name: "nodejs : read file",
+        reqObject: {
+            language: "nodejs",
+            script: 
+                "const fs = require('fs');\n" +
+                "fs.readFile('test.txt', 'utf8', (err, data) => {\n" +
+                "    if (err) throw err;\n" +
+                "    console.log(data);\n" +
+                "});"
+        },
+        fileContent: {
+            "test.txt": "This is a test file."
+        },
+        expectedResponse: {
+            val: "This is a test file.",
+            status: 200,
+            error: 0
+        }
+    },
+    {
+        name: "nodejs : write file",
+        reqObject: {
+            language: "nodejs",
+            script: 
+                "const fs = require('fs');\n" +
+                "fs.writeFile('output.txt', 'Hello, world!', 'utf8', (err) => {\n" +
+                "    if (err) throw err;\n" +
+                "    console.log('File has been saved!');\n" +
+                "});"
+        },
+        expectedResponse: {
+            val: "File has been saved!",
+            status: 200,
+            error: 0
+        },
+        checkFileContent: {
+            "output.txt": "Hello, world!"
+        }
+    },
+    
+    
+    {
         name: 'python : hello world',
         reqObject: {
             language: 'python',
@@ -82,6 +209,62 @@ const testCases = [
             error: 0,
         },
     },
+
+    {
+        name: "python : for loop",
+        reqObject: {
+            language: "python",
+            script: 
+                "for i in range(5):\n" +
+                "    print(i)"
+        },
+        expectedResponse: {
+            val: "0\n1\n2\n3\n4\n",
+            status: 200,
+            error: 0
+        }
+    },
+
+    {
+        name: "python : read file",
+        reqObject: {
+            language: "python",
+            script: 
+                "with open('test.txt', 'r') as file:\n" +
+                "    data = file.read()\n" +
+                "print(data)"
+        },
+        fileContent: {
+            "test.txt": "This is a test file."
+        },
+        expectedResponse: {
+            val: "This is a test file.",
+            status: 200,
+            error: 0
+        }
+    },
+
+    {
+        name: "python : write file",
+        reqObject: {
+            language: "python",
+            script: 
+                "with open('output.txt', 'w') as file:\n" +
+                "    file.write('Hello, world!')\n" +
+                "print('File has been saved!')"
+        },
+        expectedResponse: {
+            val: "File has been saved!",
+            status: 200,
+            error: 0
+        },
+        checkFileContent: {
+            "output.txt": "Hello, world!"
+        }
+    },
+    
+    
+    
     {
         name: 'python : print stdin',
         reqObject: {
@@ -120,6 +303,80 @@ const testCases = [
             error: 0,
         },
     },
+
+    {
+        name: "c : conditional statements",
+        reqObject: {
+            language: "c",
+            script: 
+                "#include <stdio.h>\n" +
+                "int main() {\n" +
+                "    int a = 10;\n" +
+                "    if (a > 5) {\n" +
+                "        printf(\"a is greater than 5\\n\");\n" +
+                "    } else {\n" +
+                "        printf(\"a is not greater than 5\\n\");\n" +
+                "    }\n" +
+                "    return 0;\n" +
+                "}\n"
+        },
+        expectedResponse: {
+            val: "a is greater than 5\n",
+            status: 200,
+            error: 0
+        }
+    },
+
+    {
+        name: "c : for loop",
+        reqObject: {
+            language: "c",
+            script: 
+                "#include <stdio.h>\n" +
+                "int main() {\n" +
+                "    for (int i = 0; i < 5; ++i) {\n" +
+                "        printf(\"%d\\n\", i);\n" +
+                "    }\n" +
+                "    return 0;\n" +
+                "}\n"
+        },
+        expectedResponse: {
+            val: "0\n1\n2\n3\n4\n",
+            status: 200,
+            error: 0
+        }
+    },
+    
+    {
+        name: "c : write file",
+        reqObject: {
+            language: "c",
+            script: 
+                "#include <stdio.h>\n" +
+                "#include <stdlib.h>\n" +
+                "int main() {\n" +
+                "    FILE *file = fopen(\"output.txt\", \"w\");\n" +
+                "    if (file == NULL) {\n" +
+                "        fprintf(stderr, \"Could not open file\\n\");\n" +
+                "        return 1;\n" +
+                "    }\n" +
+                "    fprintf(file, \"Hello, world!\\n\");\n" +
+                "    fclose(file);\n" +
+                "    printf(\"File has been saved!\\n\");\n" +
+                "    return 0;\n" +
+                "}\n"
+        },
+        expectedResponse: {
+            val: "File has been saved!\n",
+            status: 200,
+            error: 0
+        },
+        checkFileContent: {
+            "output.txt": "Hello, world!\n"
+        }
+    },
+    
+    
     {
         name: 'c : print stdin',
         reqObject: {
@@ -160,6 +417,70 @@ const testCases = [
         },
     },
     {
+        name: "java : simple arithmetic",
+        reqObject: {
+            language: "java",
+            script: 
+                "public class Main {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        int a = 5;\n" +
+                "        int b = 10;\n" +
+                "        System.out.println(a + b);\n" +
+                "    }\n" +
+                "}\n"
+        },
+        expectedResponse: {
+            val: "15\n",
+            status: 200,
+            error: 0
+        }
+    },
+    
+
+    {
+        name: "java : conditional statements",
+        reqObject: {
+            language: "java",
+            script: 
+                "public class Main {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        int a = 10;\n" +
+                "        if (a > 5) {\n" +
+                "            System.out.println(\"a is greater than 5\");\n" +
+                "        } else {\n" +
+                "            System.out.println(\"a is not greater than 5\");\n" +
+                "        }\n" +
+                "    }\n" +
+                "}\n"
+        },
+        expectedResponse: {
+            val: "a is greater than 5\n",
+            status: 200,
+            error: 0
+        }
+    },
+    {
+        name: "java : for loop",
+        reqObject: {
+            language: "java",
+            script: 
+                "public class Main {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        for (int i = 0; i < 5; i++) {\n" +
+                "            System.out.println(i);\n" +
+                "        }\n" +
+                "    }\n" +
+                "}\n"
+        },
+        expectedResponse: {
+            val: "0\n1\n2\n3\n4\n",
+            status: 200,
+            error: 0
+        }
+    },
+    
+
+    {
         name: 'java : print stdin',
         reqObject: {
             language: 'java',
@@ -196,6 +517,62 @@ const testCases = [
             error: 0,
         },
     },
+
+    {
+        "name": "ruby : conditional statements",
+        "reqObject": {
+            "language": "ruby",
+            "script": 
+                "a = 10\n" +
+                "if a > 5\n" +
+                "    puts 'a is greater than 5'\n" +
+                "else\n" +
+                "    puts 'a is not greater than 5'\n" +
+                "end"
+        },
+        expectedResponse: {
+            val: "a is greater than 5\n",
+            status: 200,
+            error: 0
+        }
+    },
+
+    {
+        name: "ruby : division by zero error",
+        reqObject: {
+            language: "ruby",
+            script: 
+                "begin\n" +
+                "    5 / 0\n" +
+                "rescue => e\n" +
+                "    puts 'Error occurred: ' + e.message\n" +
+                "end"
+        },
+        expectedResponse: {
+            val: "Error occurred: divided by 0\n",
+            status: 200,
+            error: 0
+        }
+    },
+
+    {
+        name: "ruby : for loop",
+        reqObject: {
+            language: "ruby",
+            script: 
+                "for i in 0..4\n" +
+                "    puts i\n" +
+                "end"
+        },
+        expectedResponse: {
+            val: "0\n1\n2\n3\n4\n",
+            status: 200,
+            error: 0
+        }
+    },
+    
+    
+    
     {
         name: 'ruby : print stdin',
         reqObject: {
