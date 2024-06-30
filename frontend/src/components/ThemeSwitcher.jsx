aa6b71e2607043f8555212e4f0eaf60c355bfba1
Vimal Vs <vimalchan24@gmail.com>
2024-06-30 23:45:54 +0530

    border added

--- frontend/src/components/ThemeSwitcher.jsx
+++ frontend/src/components/ThemeSwitcher.jsx
@@ -17,11 +17,11 @@ export default function ThemeSwitcher() {
     return (
         <>
             {theme === "light" ? (
-                <Button variant="ghost" className="" onClick={() => setTheme("dark")}>
+                <Button variant="ghost" className="border border-black" onClick={() => setTheme("dark")}>
                     <MoonIcon className="size-[1rem]" />
                 </Button>
             ) : (
-                <Button variant="ghost" className="hover:bg-slate-100 dark:hover:bg-slate-900" onClick={() => setTheme("light")}>
+                <Button variant="ghost" className="border border-gray-300 hover:bg-slate-100 dark:hover:bg-slate-900" onClick={() => setTheme("light")}>
                     <SunIcon className="size-[1rem] text-black dark:text-white" />
                 </Button>
             )}
