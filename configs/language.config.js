d69cb695ca808937b706328b43418265067ccef8
TUSHAR TOMAR <122224857+tushartomarsrm@users.noreply.github.com>
2024-07-01 03:40:44 +0530

    Update language.config.js

--- configs/language.config.js
+++ configs/language.config.js
@@ -33,13 +33,35 @@ const LANGUAGES_CONFIG = {
         filename: 'Solution.java',
         memory: ALLOWED_RAM * ONE_MB,
     },
+    [GO]: {
+    compile: "go build -o a.out solution.go",
+    run: "./a.out",
+    timeout: 2,
+    filename: "solution.go",
+    memory: ALLOWED_RAM * ONE_MB,
+  },
+  [CSHARP]: {
+    compile:
+      "dotnet new console -o myapp && mv Program.cs myapp/Program.cs && dotnet build myapp",
+    run: "dotnet myapp/bin/Debug/net7.0/myapp.dll",
+    timeout: 10,
+    filename: "Program.cs",
+    memory: ALLOWED_RAM * ONE_MB,
+  },
+  [KOTLIN]: {
+    compile: "kotlinc solution.kt -include-runtime -d solution.jar",
+    run: "java -jar solution.jar",
+    timeout: 4,
+    filename: "solution.kt",
+      memory: ALLOWED_RAM * ONE_MB,
+  },
     [NODEJS]: {
         compile: 'node --check solution.js',
         run: 'node solution.js',
         timeout: 10,
         filename: 'solution.js',
-        memory: 786432, // Node.js v20 requires more initial memory, so initialize it to around 780MB (1.5 * 512MB). This value is higher than the previous 512MB but below 1GB to ensure ulimit catches excessive memory use without the GCR container being killed.
-    },
+        memory: ALLOWED_RAM * ONE_MB,
+  },
     [RUBY]: {
         compile: 'ruby -c solution.rb',
         run: 'ruby solution.rb',
