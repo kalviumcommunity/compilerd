e917602f5539dbbb4b2cd0e7325bb735bea22d21
Ankush <119110909+Ezazo21@users.noreply.github.com>
2024-07-01 09:02:14 +0530

    Update language.config.js

--- configs/language.config.js
+++ configs/language.config.js
@@ -1,4 +1,4 @@
-const { CPP, C, PYTHON, JAVA, NODEJS, RUBY, PROMPTV1, PROMPTV2 } = require('../enums/supportedLanguages')
+const { CPP, C, PYTHON, JAVA, NODEJS, RUBY,FORTRAN,PERL,ADA,PROMPTV1, PROMPTV2 } = require('../enums/supportedLanguages')
 const ONE_MB = 1024 // ulimit uses Kilobyte as base unit
 const ALLOWED_RAM = process.env.ALLOWED_RAM || 512
 
@@ -43,6 +43,27 @@ const LANGUAGES_CONFIG = {
         run: 'ruby solution.rb',
         timeout: 10,
         filename: 'solution.rb',
+        memory: ALLOWED_RAM * ONE_MB,
+    },
+    [FORTRAN]: {
+        compile: 'gfortran -o a.out solution.f90',
+        run: './a.out',
+        timeout: 2,
+        filename: 'solution.f90',
+        memory: ALLOWED_RAM * ONE_MB,
+    },
+    [PERL]: {
+        compile: 'perl -c solution.pl',
+        run: 'perl solution.pl',
+        timeout: 10,
+        filename: 'solution.pl',
+        memory: ALLOWED_RAM * ONE_MB,
+    },
+    [ADA]: {
+        compile: 'gnatmake -o a.out solution.adb',
+        run: './a.out',
+        timeout: 2,
+        filename: 'solution.adb',
         memory: ALLOWED_RAM * ONE_MB,
     },
     [PROMPTV1]: {
