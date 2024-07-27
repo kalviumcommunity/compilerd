ac5dd8062658808d6347729de5996665fb14b2c1
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-07-27 20:48:08 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,4 +1,17 @@
-const express = require('express')
+const express = require('express');
+const app = express();  
+const codeRouter = require('./routers/code.router');
+
+app.use('/', codeRouter);  
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
+
+/*const express = require('express')
 const router = express.Router()
 
 const codeRouter = require('./routers/code.router')
@@ -6,3 +19,4 @@ const codeRouter = require('./routers/code.router')
 router.use('/', codeRouter)
 
 module.exports = router
+*/
