7c5e1aa5a51dc66df1d8b0ddea871a76cefabd17
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-12-12 18:18:28 +0530

    chore: add frontend_react_jasmine_v2 in supported templates

--- enums/supportedPMFTypes.js
+++ enums/supportedPMFTypes.js
@@ -1,5 +1,6 @@
 module.exports = {
     FRONTEND_STATIC_JASMINE: 'frontend_static_jasmine',
     FRONTEND_REACT_JASMINE: 'frontend_react_jasmine',
+    FRONTEND_REACT_JASMINE_V2: 'frontend_react_jasmine_v2',
     NODEJS_JUNIT: 'nodejs_junit',
 }
